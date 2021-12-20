bbb24d41cb731949e942b732cb997ee2f183398b
Oğuzhan Beyaz <36895904+oguzbeyaz06@users.noreply.github.com>
2021-12-20 18:26:36 +0300

    fib and support added

--- bot/fonksiyonlar.py
+++ bot/fonksiyonlar.py
@@ -1,10 +1,10 @@
 import ccxt
 # import plotly.express as px
 # import plotly.graph_objects as go
-# import pandas as pd
+import pandas as pd
 import requests
-# import matplotlib.pyplot as plt
-# import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 class funcAnalysis():
@@ -16,6 +16,45 @@ class funcAnalysis():
             'https://api.whale-alert.io/v1/transactions?api_key=H1722a89y8XBb4mRzUpZfPiCFXPpVE3r&min_value=500000&limit=50')
         res = req.json()
         return res
+
+    def fibChart(self, sembol):
+        binance = ccxt.binance()
+        symbol = sembol
+        bars = binance.fetch_ohlcv(
+            symbol.upper()+'/USDT', timeframe='1d', limit=120)
+        df = pd.DataFrame(
+            bars, columns=['Date', 'Open', 'High', 'Low', 'Close', 'Volume'])
+        highest_swing = -1
+        lowest_swing = -1
+        for i in range(1, df.shape[0]-1):
+            if df['High'][i] > df['High'][i-1] and df['High'][i] > df['High'][i+1] and (highest_swing == -1 or df['High'][i] > df['High'][highest_swing]):
+                highest_swing = i
+            if df['Low'][i] < df['Low'][i-1] and df['Low'][i] < df['Low'][i+1] and (lowest_swing == -1 or df['Low'][i] < df['Low'][lowest_swing]):
+                lowest_swing = i
+        ratios = [0, 0.236, 0.382, 0.5, 0.618, 0.786, 1]
+        colors = ["white", "r", "g", "b", "cyan", "magenta", "yellow"]
+        levels = []
+        max_level = df['High'][highest_swing]
+        min_level = df['Low'][lowest_swing]
+        for ratio in ratios:
+            if highest_swing > lowest_swing:  # Uptrend
+                levels.append(max_level - (max_level-min_level)*ratio)
+            else:  # Downtrend
+                levels.append(min_level + (max_level-min_level)*ratio)
+        plt.rcParams['figure.figsize'] = [12, 7]
+        plt.rc('font', size=14)
+        xs = np.linspace(1, 21, 200)
+
+        plt.style.use('dark_background')
+        plt.plot(df['Close'])
+        start_date = df.index[min(highest_swing, lowest_swing)]
+        end_date = df.index[max(highest_swing, lowest_swing)]
+        for i in range(len(levels)):
+            plt.hlines(levels[i], start_date, end_date, label="{:.1f}%".format(
+                ratios[i]*100), colors=colors[i], linestyles="dashed")
+        plt.legend()
+        plt.savefig('fib.png')
+        plt.close()
 
     def bitcoinBubbleIndex(self):
         req = requests.get(
--- bot/fundingBot.py
+++ bot/fundingBot.py
@@ -1,12 +1,18 @@
 import discord
-import requests
-import json
+# import requests
+# import json
 import ccxt
-import time  # botu durduruyor kullanma
+# import time  # botu durduruyor kullanma
 from tinydb import TinyDB, Query
+import matplotlib.pyplot as plt
+from tinydb.operations import delete
 import asyncio
+from tinydb.queries import where
 import fonksiyonlar
 import datetime
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
 
 TOKEN = 'OTE0ODQ5MTIyMzgzNzI0NTU1.YaTBow.ouOKIutxB3GAHzoPPMVSLMf_Z08'
 edatabase = TinyDB('degisim.json')
@@ -14,6 +20,55 @@ exchange = ccxt.binance()
 client = discord.Client()
 CALCULATE_PUAN = 0
 func = fonksiyonlar.funcAnalysis()
+
+
+def isSupport(df, i):
+    support = df['Low'][i] < df['Low'][i-1] and df['Low'][i] < df['Low'][i +
+                                                                         1] and df['Low'][i+1] < df['Low'][i+2] and df['Low'][i-1] < df['Low'][i-2]
+    return support
+
+
+def isResistance(df, i):
+    resistance = df['High'][i] > df['High'][i-1] and df['High'][i] > df['High'][i +
+                                                                                1] and df['High'][i+1] > df['High'][i+2] and df['High'][i-1] > df['High'][i-2]
+    return resistance
+
+
+def get_chart(pair, time):
+    binance = ccxt.binance()
+    trading_pair = pair
+    trading_time = time
+    candles = binance.fetch_ohlcv(trading_pair, str(trading_time), limit=120)
+    data = pd.DataFrame(candles, columns=['Date',
+                                          'Open', 'High', 'Low', 'Close', 'Volume'])
+    data = data.iloc[::-1]
+    data['Date'] = pd.to_datetime(data['Date'], unit='ms')
+    fig = go.Figure(data=[go.Candlestick(x=data['Date'], open=data['Open'],
+                                         high=data['High'], low=data['Low'], close=data['Close'], increasing_line_color='gray', decreasing_line_color='#C00000')])
+    fig.update_layout(xaxis_rangeslider_visible=False,
+                      template='plotly_dark', title=pair + ' - ' + str(trading_time))
+    levels = []
+    s = np.mean(data['High'] - data['Low'])
+
+    def isFarFromLevel(l):
+        return np.sum([abs(l-x) < s for x in levels]) == 0
+    levels = []
+    for i in range(2, data.shape[0]-2):
+        if isSupport(data, i):
+            l = data['Low'][i]
+            if isFarFromLevel(l):
+                levels.append((i, l))
+        elif isResistance(data, i):
+            l = data['High'][i]
+            if isFarFromLevel(l):
+                levels.append((i, l))
+
+    for level in levels:
+        fig.add_hline(y=level[1], line_dash="dot", row=3, col="all", annotation_text=str(level[1]),
+                      annotation_position="bottom right"
+                      )
+
+    fig.write_image('destek.png')
 
 
 @client.event
@@ -41,7 +96,8 @@ async def background_task():
         edatabase.insert(kayit)
         fonlamaPuan = ileri['fundingRatePuan']
         cokluAnaliz = ileri['puan']
-        await channel.send(f'** Bitcoin Uyarı **\nFonlama Oranı Puanı : `{fonlamaPuan}`\nÇoklu Analiz Puan = `{cokluAnaliz}`\nFiyat = `{price}`')
+        await channel.send(f'** Bitcoin Uyarı **\nFonlama Oranı Puanı : `{fonlamaPuan}`\nÇoklu Analiz Puan = `{cokluAnaliz}`\nFiyat = `{price}`\n')
+
         await asyncio.sleep(14400)
 
 
@@ -55,6 +111,7 @@ async def on_message(message):
     if message.author == client.user:
         return
     if message.channel.name == 'analiz':
+
         if user_message.lower() == 'veri':
             data = edatabase.all()
             embedVar = discord.Embed(
@@ -133,6 +190,20 @@ async def on_message(message):
                     embedVar.add_field(
                         name=f'Nötr\n{exchange}', value=f'{rate}', inline=True)
             await message.channel.send(embed=embedVar)
-
+        # fib chart
+        if user_message.lower().split()[0] == 'fib':
+            symbol = user_message.lower().split()[1]
+            func.fibChart(symbol)
+            await message.channel.send(file=discord.File('fib.png'))
+        # destek direnç
+        if user_message.lower().split()[0] == 'grafik':
+            symbol = user_message.lower().split()[1]
+            timezone = user_message.lower().split()[2]
+            pair = symbol.upper() + 'USDT'
+            if timezone == '4' and timezone == '1':
+                await message.channel.send(' ---> 1m - 15m - 30m - 1h - 4h - 1d - 1w  <--- ')
+            else:
+                get_chart(pair, timezone)
+                await message.channel.send(file=discord.File('destek.png'))
 client.loop.create_task(background_task())
 client.run(TOKEN)
